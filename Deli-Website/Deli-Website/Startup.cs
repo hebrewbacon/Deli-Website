060071997be41a5b1a2da24202e2320d9921a632
hebrewbacon <brasewel@gmail.com>
2015-03-28 22:30:12 -0400

    Committing the project shell

---
+++ Deli-Website/Deli-Website/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Deli_Website.Startup))]
+namespace Deli_Website
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
